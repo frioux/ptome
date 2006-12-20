c90e7cf4c35fff1645e8f6189f608f54113a4c7d
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:51:47 +0000

    r562@calefactor (orig r187):  olsonc | 2006-10-07 16:37:51 -0500  Created a working test script that first blanks the database.  The script gets its connection information from command line options.  Refs #18.

--- TRUNK/scripts/test.pl
+++ TRUNK/scripts/test.pl
@@ -9,17 +9,19 @@ use Crypt::PasswdMD5;
 use IO::Prompt;
 use DBI;
 use IO::File;
+use Getopt::Long;
 
 my $address = '';
 my $port = '';
 my $username = '';
 my $password = '';
 my $database = '';
+my @args = '';
 
 GetOptions (
         'username:s' => \$username,  #-u, -user, or -username, fills $username
         'password:s' => \$password,     #-p, -pass, or -password, fills $password
-        'address:s' => \$serveraddress,    #-a, or -address, fills $passwd
+        'address:s' => \$address,    #-a, or -address, fills $passwd
         'port:s' => \$port,          #-port, fills $admin
 	'database:s' => \$database, #-d or -database, fills $database
 );
@@ -44,3 +46,15 @@ if (!$database) {
 	$database = 'tome2';
 }
 
+print ("Blanking and re-creating database \'" . $database . "\'...\n");
+
+$ENV{'PGPASSWORD'} = $password;
+@args = ("psql", "-p", "-U", $username, "-h", $address, "-p", $port, "-d", $database, "-f", '../devdocs/clean.sql');
+system (@args);
+$ENV{'PGPASSWORD'} = '';
+
+print ("\n");
+print ("Beginning tests...\n");
+
+@args = ("prove", "-r", '../tests');
+system (@args);
--- TRUNK/tests/testframework.t
+++ TRUNK/tests/testframework.t
@@ -1,12 +1,17 @@
 #Test that the testing framework works correctly
 
-use lib "../modules"
-use TOME;
+#use lib "../modules"
+#use TOME;
 use Test::More tests => 2;
 
-my $app = TOME->new();
+#my $app = TOME->new();
 
-$app->
+#$app->
+
+my $a = 2 + 2;
+my $b = 2 * 2;
+
+is ($a, $b);
 
 #Is up down?
 
