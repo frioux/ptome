2b0c19083a3fe0e72d669020e42b2763488989a5
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:37:21 +0000

    r467@calefactor (orig r132):  fjord | 2006-09-16 18:54:04 -0500   r463@calefactor:  cghawthorne | 2006-09-17 00:46:53 -0500   further work

--- TRUNK/scripts/ISBNvsID.sql
+++ TRUNK/scripts/ISBNvsID.sql
@@ -1,6 +1,10 @@
-create table reservations (id serial primary key, isbn varchar(20) references books not null, uid int references users (id) not null, patron int references patrons (id) not null, reserved timestamp with time zone not null default now(), fulfilled timestamp with time zone, comment text, library int references libraries (id) not null, semester int references semesters (id) not null);
+alter TABLE checkouts alter column tomebook type int;
+alter TABLE checkouts alter column semester type int;
+ALTER TABLE tomebooks alter column expire type integer;
 
-insert into reservations (isbn,uid,patron,reserved,comment,library,semester) select isbn, uid, borrower as patron, checkout as reserved, checkouts.comments as comment, checkouts.library as library, semester from checkouts,tomebooks where reservation = true AND checkouts.tomebook = tomebooks.id;
+create table reservations (id serial primary key, isbn varchar(20) references books not null, uid int references users (id) not null, patron int references patrons (id) not null, reserved timestamp with time zone not null default now(), fulfilled timestamp with time zone, comment text, library_from int references libraries (id) not null, library_to int references libraries (id), semester int references semesters (id) not null);
+
+insert into reservations (isbn,uid,patron,reserved,comment,library_from, library_to,semester) select isbn, uid, borrower as patron, checkout as reserved, checkouts.comments as comment, checkouts.library as library_from, tomebooks.library as library_to, semester from checkouts,tomebooks where reservation = true AND checkouts.tomebook = tomebooks.id;
 
 delete from checkouts where reservation = true;
 drop INDEX one_borrower;
@@ -10,3 +14,10 @@ ALTER TABLE checkouts drop column reservation;
 CREATE UNIQUE INDEX one_borrower ON checkouts (tomebook) WHERE checkin IS NULL;
 ALTER TABLE reservations ADD check(fulfilled IS NULL OR fulfilled > reserved);
 
+create function tomebooks_available_to_reserve(books.isbn%TYPE, tomebooks.library%TYPE, checkouts.semester%TYPE) returns bigint AS $$
+select count(*) from tomebooks where library = $2 AND isbn = $1 AND timeremoved IS NULL AND id NOT IN (SELECT checkouts.tomebook FROM checkouts,tomebooks WHERE semester = $3 AND tomebooks.isbn = $1 AND checkin IS NULL AND tomebooks.library = $2 AND tomebooks.id = checkouts.tomebook);
+$$ LANGUAGE SQL;
+
+create function tomebooks_reserved(books.isbn%TYPE, tomebooks.library%TYPE, checkouts.semester%TYPE) returns bigint AS $$
+select count(*) from reservations where isbn = $1 AND library_to = $2 AND semester = $3;
+$$ LANGUAGE SQL;
