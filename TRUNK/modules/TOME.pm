00b6a291e74fc818e24e6000241701891fb10fc0
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:30:38 +0000

    r428@calefactor (orig r105):  frew | 2006-08-02 09:06:27 -0500  Added pod and vim folding stuff

--- TRUNK/modules/TOME.pm
+++ TRUNK/modules/TOME.pm
@@ -1,5 +1,28 @@
 package TOME;
 
+#{{{pod
+
+=head1 NAME
+
+TOME
+
+=cut
+
+=head1 SYNOPSIS
+
+This is the main module for all TOME interactions.
+
+=cut
+
+=head1 DESCRIPTION
+
+I really don't know how all of this works...
+
+=cut
+
+#}}}
+
+#{{{ use's
 use base 'CGI::Application';
 
 use TOME::TemplateCallbacks;
@@ -17,7 +40,63 @@ use MIME::Lite;
 
 use strict;
 use warnings;
+#}}}
 
+#{{{ CONFIG
+
+=head2 CONFIG
+
+The CONFIG hash describes various and sundry configurata.
+
+These configurata are:
+
+=over
+
+=item cgibase
+
+the relative location of the cgi stuff
+
+=item staticbase
+
+unsure
+
+=item templatepath 
+
+the relative location of the templates
+
+=item dbidatasource 
+
+the database source.  funky format, description please?
+
+=item dbiusername
+
+username for the database
+
+=item dbipassword
+
+password for the database
+
+=item notifyfrom
+
+the "address" email messages are from
+
+=item adminemail
+
+the "address" email messages are sent to
+
+=item devmode
+
+whether or not this is being run in development mode
+
+=item devemailto
+
+the "address" email messages are sent to when in development mode
+
+=back
+
+Note: address format is 'Name <email@domain>`
+
+=cut
 our %CONFIG = (
 	cgibase		=> '/perl/tome/cgi',
 	staticbase	=> '/tome',
@@ -34,10 +113,21 @@ our %CONFIG = (
 	devmode		=> 0,
 	devemailto	=> 'TOMEadmin <tomeadmin@tome>',
 );
+#}}}
 
 require '../site-config.pl';
 
 $ENV{PATH} = '/usr/sbin:/usr/bin:/sbin:/bin';
+
+
+#{{{ cgiapp_init
+
+
+=head2 cgiapp_init
+
+This appears to (and probably does considering the name) set up various variables that are needed for CGI stuff.  It takes no arguments.
+
+=cut
 
 sub cgiapp_init {
 	my $self = shift;
@@ -56,6 +146,24 @@ sub cgiapp_init {
 	$self->param('semesters', $self->semester_info);
 	$self->param('currsemester', $self->semester_info({ current => 'true' }));
 }
+#}}}
+
+#{{{ error_runmode
+
+=head2 error_runmode
+
+This is called when there is an error that we really don't know what caused.
+This takes one argument:
+
+=over
+
+=item error
+
+the error message (I presume)
+
+=back
+
+=cut
 
 sub error_runmode {
 	my $self = shift;
@@ -75,6 +183,32 @@ sub error_runmode {
 
 	return $self->error({ message => "Internal exception error", extended => $debug });
 }
+#}}}
+
+#{{{ error
+=head2 error
+
+error
+
+=head2 error
+
+This function is called when there is a fatal error.
+
+This function takes a hash as an argument:
+
+=over
+
+=item message
+
+the short error message
+
+=item extended
+
+the details of the error message
+
+=back
+
+=cut
 
 sub error {
 	my $self = shift;
@@ -92,6 +226,49 @@ sub error {
 
 	return $output;
 }
+#}}}
+
+#{{{tomebooks_search 
+
+=head2 tomebooks_search 
+
+This returns an array of found textbooks.
+
+It takes arguments in the form of a hash:
+
+=over
+
+=item isbn
+
+the isbn to look for
+
+=item status
+
+that status of the book (all, can_reserve, can_checkout, or in_collection)
+
+=item title
+
+the title of the book
+
+=item author
+
+the author of the book
+
+=item  edition
+
+the edition of the book
+
+=item libraries
+
+the libraries to look in for the book (Note: this is an array reference...whatever thatmeans.)
+
+=item semester
+
+the semester that the book is here for???
+
+=back
+
+=cut
 
 sub tomebooks_search {
 	my $self = shift;
@@ -171,6 +348,29 @@ sub tomebooks_search {
 	
 	return @results;
 }
+#}}}
+
+#{{{expire_search 
+
+=head2 expire_search 
+
+This function returns a list of books that will expire on the given semester with the given libraries.
+
+The arguments are given as a hash:
+
+=over
+
+=item semester
+
+The semester in which the books will expire
+
+=item libraries
+
+The libraries the books reside in
+
+=back
+
+=cut
 
 sub expire_search {
 	my $self = shift;
@@ -192,6 +392,29 @@ sub expire_search {
 	}
 	return \@results;
 }
+#}}}
+
+#{{{reservation_search 
+
+=head2 reservation_search 
+
+This function returns an array of books that are reserved in a given semester from given libraries.
+
+The arguments are given as a hash:
+
+=over
+
+=item semester
+
+the semester that the books are reserved for
+
+=item libraries
+
+the libraries that the books are in
+
+=back
+
+=cut
 
 sub reservation_search {
 	my $self = shift;
@@ -213,6 +436,29 @@ sub reservation_search {
 	}
 	return \@results;
 }
+#}}}
+
+#{{{dueback_search 
+
+=head2 dueback_search 
+
+This function returns a list of books that are due back at the given semester from the given libraries.
+
+The arguments are given as a hash:
+
+=over
+
+=item semester
+
+the semester that the books are due back
+
+=item libraries
+
+the libraries that the books are in
+
+=back
+
+=cut
 
 sub dueback_search {
 	my $self = shift;
@@ -234,6 +480,37 @@ sub dueback_search {
 	}
 	return \@results;
 }
+#}}}
+
+#{{{add_book 
+
+=head2 add_book 
+
+This function is used to add a book to TOME.  This is not for adding a real book, but for adding a book type.  Like adding a Class, not an Object.
+
+This function takes a hash as an argument:
+
+=over
+
+=item isbn
+
+The ISBN of the book
+
+=item title
+
+The Title of the book
+
+=item author
+
+The Author of the book
+
+=item edition
+
+The Edition of the book
+
+=back
+
+=cut
 
 sub add_book {
 	my $self = shift;
@@ -252,6 +529,29 @@ sub add_book {
 	my $sth = $dbh->prepare("INSERT INTO books (isbn, title, author, edition) VALUES (?, ?, ?, ?)");
 	$sth->execute($params{isbn}, $params{title}, $params{author}, $params{edition});
 }
+#}}}
+
+#{{{add_class 
+
+=head2 add_class 
+
+This function adds a class to TOME.
+
+The arguments are given as a hash:
+
+=over
+
+=item id
+
+The "School Readable" name of the class (that is, something like, MATH9999)
+
+=item name
+
+The Human Readable name of the Class
+
+=back
+
+=cut
 
 sub add_class {
 	my $self = shift;
@@ -265,6 +565,33 @@ sub add_class {
 
 	$dbh->do("INSERT INTO classes (id, name) VALUES (?, ?)", undef, @params{qw(id name)});
 }
+#}}}
+
+#{{{patrons_search 
+
+=head2 patrons_search 
+
+This function finds patrons.
+
+Arguments are given as a hash:
+
+=over
+
+=item id
+
+The Nickname of the patron
+
+=item name
+
+The Real Name of the patron
+
+=item email
+
+The Email of the patron
+
+=back
+
+=cut
 
 sub patrons_search {
 	my $self = shift;
@@ -316,6 +643,15 @@ sub patrons_search {
 	
 	return @results;
 }
+#}}}
+
+#{{{patron_add 
+
+=head2 patron_add 
+
+foo
+
+=cut
 
 sub patron_add {
 	my $self = shift;
@@ -329,6 +665,15 @@ sub patron_add {
 	my $sth = $self->dbh->prepare($sql);
 	$sth->execute(@bind);
 }
+#}}}
+
+#{{{patron_update 
+
+=head2 patron_update 
+
+foo
+
+=cut
 
 sub patron_update {
 	my $self = shift;
@@ -343,6 +688,15 @@ sub patron_update {
 	my $sth = $self->dbh->prepare($sql);
 	$sth->execute(@bind);
 }
+#}}}
+
+#{{{patron_info 
+
+=head2 patron_info 
+
+foo
+
+=cut
 
 sub patron_info {
 	my $self = shift;
@@ -365,7 +719,15 @@ sub patron_info {
 	$sth->execute(@bind);
 	return $sth->fetchrow_hashref;
 }
-		
+#}}}
+
+#{{{books_search 
+
+=head2 books_search 
+
+foo
+
+=cut
 
 sub books_search {
 	my $self = shift;
@@ -412,6 +774,15 @@ sub books_search {
 	
 	return @results;
 }
+#}}}
+
+#{{{book_exists 
+
+=head2 book_exists 
+
+foo
+
+=cut
 
 sub book_exists {
 	my $self = shift;
@@ -430,6 +801,15 @@ sub book_exists {
 	my ($count) = $sth->fetchrow_array;
 	return $count;
 }
+#}}}
+
+#{{{book_classes 
+
+=head2 book_classes 
+
+foo
+
+=cut
 
 sub book_classes {
 	my $self = shift;
@@ -450,6 +830,15 @@ sub book_classes {
 
 	return \@results;
 }
+#}}}
+
+#{{{add_tomebook 
+
+=head2 add_tomebook 
+
+foo
+
+=cut
 
 sub add_tomebook {
 	my $self = shift;
@@ -481,6 +870,15 @@ sub add_tomebook {
 	my ($id) = $dbh->selectrow_array("SELECT currval('public.tomebooks_id_seq')");
 	return $id;
 }
+#}}}
+
+#{{{classbook_add 
+
+=head2 classbook_add 
+
+foo
+
+=cut
 
 sub classbook_add {
 	my $self = shift;
@@ -497,6 +895,15 @@ sub classbook_add {
 	my ($sql, @bind) = sql_interp('INSERT INTO classbooks', \%params);
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{tomebook_info 
+
+=head2 tomebook_info 
+
+foo
+
+=cut
 
 sub tomebook_info {
 	my $self = shift;
@@ -514,6 +921,15 @@ sub tomebook_info {
 
 	return $sth->fetchrow_hashref();
 }
+#}}}
+
+#{{{tomebook_info_deprecated 
+
+=head2 tomebook_info_deprecated 
+
+foo
+
+=cut
 
 sub tomebook_info_deprecated {
 	my $self = shift;
@@ -536,6 +952,15 @@ sub tomebook_info_deprecated {
 
 	return $results;
 }
+#}}}
+
+#{{{tome_stats 
+
+=head2 tome_stats 
+
+foo
+
+=cut
 
 sub tome_stats {
 	my $self = shift;
@@ -573,6 +998,15 @@ sub tome_stats {
 
 	return \%stats;
 }
+#}}}
+
+#{{{book_update 
+
+=head2 book_update 
+
+foo
+
+=cut
 
 sub book_update {
 	my $self = shift;
@@ -587,6 +1021,15 @@ sub book_update {
 	my $dbh = $self->dbh;
 	$dbh->do('UPDATE books SET title = ?, author = ?, edition = ? WHERE isbn = ?', undef, @params{qw(title author edition isbn)});
 }
+#}}}
+
+#{{{tomebook_update 
+
+=head2 tomebook_update 
+
+foo
+
+=cut
 
 sub tomebook_update {
 	my $self = shift;
@@ -608,6 +1051,15 @@ sub tomebook_update {
 		$dbh->do('UPDATE tomebooks SET originator = ?, comments = ?, library = ? WHERE id = ?', undef, @params{qw(originator comments library id)});
 	}
 }
+#}}}
+
+#{{{patron_checkouts 
+
+=head2 patron_checkouts 
+
+foo
+
+=cut
 
 sub patron_checkouts {
 	my $self = shift;
@@ -636,6 +1088,15 @@ sub patron_checkouts {
 
 	return \@results;
 }
+#}}}
+
+#{{{checkout_info 
+
+=head2 checkout_info 
+
+foo
+
+=cut
 
 sub checkout_info {
 	my $self = shift;
@@ -653,6 +1114,15 @@ sub checkout_info {
 
 	return $sth->fetchrow_hashref();
 }
+#}}}
+
+#{{{checkout_history 
+
+=head2 checkout_history 
+
+foo
+
+=cut
 
 sub checkout_history {
 	my $self = shift;
@@ -677,6 +1147,15 @@ sub checkout_history {
 
 	return \@results;
 }
+#}}}
+
+#{{{find_orphans 
+
+=head2 find_orphans 
+
+foo
+
+=cut
 
 sub find_orphans {
 	my $self = shift;
@@ -697,6 +1176,15 @@ sub find_orphans {
 
 	return @results;
 }
+#}}}
+
+#{{{find_useless 
+
+=head2 find_useless 
+
+foo
+
+=cut
 
 sub find_useless {
 	my $self = shift;
@@ -718,6 +1206,15 @@ sub find_useless {
 
 	return @results;
 }
+#}}}
+
+#{{{tomebook_checkout 
+
+=head2 tomebook_checkout 
+
+foo
+
+=cut
 
 sub tomebook_checkout {
 	my $self = shift;
@@ -737,6 +1234,15 @@ sub tomebook_checkout {
 	my ($id) = $self->dbh->selectrow_array("SELECT currval('public.checkouts_id_seq')");
 	return $id;
 }
+#}}}
+
+#{{{tomebook_can_checkout 
+
+=head2 tomebook_can_checkout 
+
+foo
+
+=cut
 
 sub tomebook_can_checkout {
 	my $self = shift;
@@ -758,6 +1264,15 @@ sub tomebook_can_checkout {
 
 	return 0;
 }
+#}}}
+
+#{{{tomebook_can_reserve 
+
+=head2 tomebook_can_reserve 
+
+foo
+
+=cut
 
 sub tomebook_can_reserve {
 	my $self = shift;
@@ -779,6 +1294,15 @@ sub tomebook_can_reserve {
 
 	return 0;
 }
+#}}}
+
+#{{{tomebook_fill_reservation 
+
+=head2 tomebook_fill_reservation 
+
+foo
+
+=cut
 
 sub tomebook_fill_reservation {
 	my $self = shift;
@@ -791,6 +1315,15 @@ sub tomebook_fill_reservation {
 
 	$dbh->do('UPDATE checkouts set checkout = now(), reservation = FALSE WHERE id = ?', undef, @params{qw(id)});
 }
+#}}}
+
+#{{{tomebook_cancel_checkout 
+
+=head2 tomebook_cancel_checkout 
+
+foo
+
+=cut
 
 sub tomebook_cancel_checkout {
 	my $self = shift;
@@ -803,6 +1336,15 @@ sub tomebook_cancel_checkout {
 
 	$dbh->do('DELETE FROM checkouts WHERE id = ?', undef, @params{qw(id)});
 }
+#}}}
+
+#{{{tomebook_checkin 
+
+=head2 tomebook_checkin 
+
+foo
+
+=cut
 
 sub tomebook_checkin {
 	my $self = shift;
@@ -816,6 +1358,15 @@ sub tomebook_checkin {
 	$dbh->do('UPDATE checkouts set reservation = FALSE WHERE id = ?', undef, @params{qw(id)}); # Obviously can't be reserved if it's checked in...
 	$dbh->do('UPDATE checkouts SET checkin = now() WHERE id = ?', undef, @params{qw(id)});
 }
+#}}}
+
+#{{{tomebook_remove 
+
+=head2 tomebook_remove 
+
+foo
+
+=cut
 
 sub tomebook_remove {
 	my $self = shift;
@@ -833,6 +1384,15 @@ sub tomebook_remove {
 		$self->dbh->do($sql, undef, @bind);
 	}
 }
+#}}}
+
+#{{{update_checkout_comments 
+
+=head2 update_checkout_comments 
+
+foo
+
+=cut
 
 sub update_checkout_comments {
 	my $self = shift;
@@ -846,6 +1406,15 @@ sub update_checkout_comments {
 
 	$dbh->do('UPDATE checkouts SET comments = ? WHERE id = ?', undef, $params{comments}, $params{id});
 }
+#}}}
+
+#{{{book_info 
+
+=head2 book_info 
+
+foo
+
+=cut
 
 sub book_info {
 	my $self = shift;
@@ -860,6 +1429,15 @@ sub book_info {
 	$sth->execute($params{isbn});
 	return $sth->fetchrow_hashref;
 }
+#}}}
+
+#{{{class_search 
+
+=head2 class_search 
+
+foo
+
+=cut
 
 sub class_search {
 	my $self = shift;
@@ -896,6 +1474,15 @@ sub class_search {
 	
 	return \@results;
 }
+#}}}
+
+#{{{class_list 
+
+=head2 class_list 
+
+foo
+
+=cut
 
 sub class_list {
 	my $self = shift;
@@ -911,6 +1498,15 @@ sub class_list {
 
 	return \@results;
 }
+#}}}
+
+#{{{class_info 
+
+=head2 class_info 
+
+foo
+
+=cut
 
 sub class_info {
 	my $self = shift;
@@ -934,6 +1530,15 @@ sub class_info {
 
 	return { name => $name, comments => $comments, books => \@books };
 }
+#}}}
+
+#{{{class_update_comments 
+
+=head2 class_update_comments 
+
+foo
+
+=cut
 
 sub class_update_comments {
 	my $self = shift;
@@ -947,6 +1552,15 @@ sub class_update_comments {
 
 	$dbh->do('UPDATE classes SET comments = ? WHERE id = ?', undef, $params{comments}, $params{id});
 }
+#}}}
+
+#{{{classbook_update 
+
+=head2 classbook_update 
+
+foo
+
+=cut
 
 sub classbook_update {
 	my $self = shift;
@@ -964,6 +1578,15 @@ sub classbook_update {
 	my ($sql, @bind) = sql_interp('UPDATE classbooks SET', { usable => $params{usable}, verified => $params{verified}, comments => $params{comments}, uid => $params{uid} }, 'WHERE', { class => $params{class}, isbn => $params{isbn} });
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{class_delete 
+
+=head2 class_delete 
+
+foo
+
+=cut
 
 sub class_delete {
 	my $self = shift;
@@ -977,6 +1600,15 @@ sub class_delete {
 	$dbh->do('DELETE FROM classbooks WHERE class = ?', undef, @params{qw(id)});
 	$dbh->do('DELETE FROM classes WHERE id = ?', undef, @params{qw(id)});
 }
+#}}}
+
+#{{{class_delete_book 
+
+=head2 class_delete_book 
+
+foo
+
+=cut
 
 sub class_delete_book {
 	my $self = shift;
@@ -990,6 +1622,15 @@ sub class_delete_book {
 
 	$dbh->do('DELETE FROM classbooks WHERE class = ? AND isbn = ?', undef, @params{qw(class isbn)});
 }
+#}}}
+
+#{{{user_info 
+
+=head2 user_info 
+
+foo
+
+=cut
 
 sub user_info {
 	my $self = shift;
@@ -1019,6 +1660,15 @@ sub user_info {
 
 	return ($params{id} || $params{username}) ? $results[0] : \@results;
 }
+#}}}
+
+#{{{user_add 
+
+=head2 user_add 
+
+foo
+
+=cut
 
 sub user_add {
 	my $self = shift;
@@ -1032,6 +1682,15 @@ sub user_add {
 	my ($sql, @bind) = sql_interp('INSERT INTO users', \%params);
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{user_update 
+
+=head2 user_update 
+
+foo
+
+=cut
 
 sub user_update {
 	my $self = shift;
@@ -1054,6 +1713,15 @@ sub user_update {
 	my ($sql, @bind) = sql_interp('UPDATE users SET', \%user, 'WHERE id =', $params{id});
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{semester_info 
+
+=head2 semester_info 
+
+foo
+
+=cut
 
 sub semester_info {
 	my $self = shift;
@@ -1077,6 +1745,15 @@ sub semester_info {
 
 	return ($params{current} eq 'true') ? $results[0] : \@results;
 }
+#}}}
+
+#{{{semester_set 
+
+=head2 semester_set 
+
+foo
+
+=cut
 
 sub semester_set {
 	my $self = shift;
@@ -1091,6 +1768,15 @@ sub semester_set {
 	$self->dbh->do($sql, undef, @bind);
 	$self->dbh->commit;
 }
+#}}}
+
+#{{{semester_add 
+
+=head2 semester_add 
+
+foo
+
+=cut
 
 sub semester_add {
 	my $self = shift;
@@ -1102,6 +1788,15 @@ sub semester_add {
 	my ($sql, @bind) = sql_interp('INSERT INTO semesters', { name => $params{name} });
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{library_add 
+
+=head2 library_add 
+
+foo
+
+=cut
 
 sub library_add {
 	my $self = shift;
@@ -1113,6 +1808,15 @@ sub library_add {
 	my ($sql, @bind) = sql_interp('INSERT INTO libraries', { name => $params{name} });
 	$self->dbh->do($sql, undef, @bind);
 }
+#}}}
+
+#{{{library_info 
+
+=head2 library_info 
+
+foo
+
+=cut
 
 sub library_info {
 	my $self = shift;
@@ -1138,6 +1842,15 @@ sub library_info {
 
 	return $params{id} ? $results[0] : \@results;
 }
+#}}}
+
+#{{{library_users 
+
+=head2 library_users 
+
+foo
+
+=cut
 
 sub library_users {
 	my $self = shift;
@@ -1156,6 +1869,15 @@ sub library_users {
 
 	return @results;
 }
+#}}}
+
+#{{{library_access 
+
+=head2 library_access 
+
+foo
+
+=cut
 
 sub library_access {
 	my $self = shift;
@@ -1191,6 +1913,15 @@ sub library_access {
 		$self->dbh->do($sql, undef, @bind);
 	}
 }
+#}}}
+
+#{{{template 
+
+=head2 template 
+
+foo
+
+=cut
 
 sub template {
 	my $self = shift;
@@ -1223,6 +1954,15 @@ sub template {
 
 	return $output;
 }
+#}}}
+
+#{{{sendmail 
+
+=head2 sendmail 
+
+foo
+
+=cut
 
 sub sendmail {
 	my $self = shift;
@@ -1247,10 +1987,45 @@ sub sendmail {
 	);
 	$message->send;
 }
+#}}}
+
+#{{{_quote_like 
+
+=head2 _quote_like 
+
+foo
+
+=cut
 
 sub _quote_like {
 	return ($_[0] =~ s/([\\%_])/\\$1/g);
 }
+#}}}
 
 1;
+
+=head1 AUTHOR
+
+Curtis Fjord Hawthornre
+
+=cut
+
+=head1 BUGS
+
+??
+
+=cut
+
+=head1 SEE ALSO
+
+postgreSQL
+
+=cut
+
+=head1 COPYRIGHT
+
+gpl?
+
+=cut
+
 
