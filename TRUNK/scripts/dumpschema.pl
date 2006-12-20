da1f23983836e9055032c608ab55b4537306bd15
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:52:03 +0000

    r563@calefactor (orig r188):  olsonc | 2006-10-07 17:01:02 -0500  Created scripts to automatically dump the TOME database with varying levels of detail  Refs #18

---
+++ TRUNK/scripts/dumpschema.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib "../modules";
+use TOME;
+use Crypt::PasswdMD5;
+use IO::Prompt;
+use DBI;
+use IO::File;
+use Getopt::Long;
+
+my $address = '';
+my $port = '';
+my $username = '';
+my $password = '';
+my $database = '';
+my $dump = '';
+
+GetOptions (
+        'username:s' => \$username,  #-u, -user, or -username, fills $username
+        'password:s' => \$password,     #-p, -pass, or -password, fills $password
+        'address:s' => \$address,    #-a, or -address, fills $passwd
+        'port:s' => \$port,          #-port, fills $admin
+        'database:s' => \$database, #-d or -database, fills $database
+);
+
+if (!$username) {
+	$username = prompt ("Username: ", -default => 'tome');
+}
+
+if (!$password) {
+	$password = prompt ("Password: ", -echo => '');
+}
+
+if (!$address) {
+	$address = prompt ("Address: ", -default => 'localhost');
+}
+
+if (!$port) {
+	$port = "5432";
+}
+
+if (!$database) {
+	$database = prompt ("Database name: ", -default => 'tome');
+}
+
+$ENV{'PGPASSWORD'} = $password;
+$dump = `pg_dump --no-owner --schema-only -h $address -p $port -U tome tome`;
+$ENV{'PGPASSWORD'} = '';
+
+print ($dump);
