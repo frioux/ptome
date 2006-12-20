51e62c18099ad13294e93aafe94f07d21321daa8
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:37:47 +0000

    r471@calefactor (orig r134):  fjord | 2006-09-17 09:13:34 -0500   r470@calefactor:  cghawthorne | 2006-09-17 15:07:54 -0500   Fixed type inconsistencies - Closes #8   Created script to transition database to new reservation system with appropriate checks - Closes #9

--- TRUNK/scripts/database_migrations/01_ISBNvsID.sql
+++ TRUNK/scripts/database_migrations/01_ISBNvsID.sql
@@ -2,7 +2,7 @@ alter TABLE checkouts alter column tomebook type int;
 alter TABLE checkouts alter column semester type int;
 ALTER TABLE tomebooks alter column expire type integer;
 
-create table reservations (id serial primary key, isbn varchar(20) references books not null, uid int references users (id) not null, patron int references patrons (id) not null, reserved timestamp with time zone not null default now(), fulfilled timestamp with time zone, comment text, library_from int references libraries (id) not null, library_to int references libraries (id), semester int references semesters (id) not null);
+create table reservations (id serial primary key, isbn varchar(20) references books not null, uid int references users (id) not null, patron int references patrons (id) not null, reserved timestamp with time zone not null default now(), fulfilled timestamp with time zone, comment text, library_from int references libraries (id) not null, library_to int references libraries (id) not null, semester int references semesters (id) not null);
 
 insert into reservations (isbn,uid,patron,reserved,comment,library_from, library_to,semester) select isbn, uid, borrower as patron, checkout as reserved, checkouts.comments as comment, checkouts.library as library_from, tomebooks.library as library_to, semester from checkouts,tomebooks where reservation = true AND checkouts.tomebook = tomebooks.id;
 
@@ -21,3 +21,29 @@ $$ LANGUAGE SQL;
 create function tomebooks_reserved(books.isbn%TYPE, tomebooks.library%TYPE, checkouts.semester%TYPE) returns bigint AS $$
 select count(*) from reservations where isbn = $1 AND library_to = $2 AND semester = $3;
 $$ LANGUAGE SQL;
+
+CREATE FUNCTION reservation_insert_update() RETURNS "trigger" AS $$
+BEGIN
+IF tomebooks_reserved(NEW.isbn, NEW.library_to, NEW.semester) + 1 <= tomebooks_available_to_reserve(NEW.isbn, NEW.library_to, NEW.semester) THEN
+return NEW;
+else
+raise exception 'All available books are already reserved';
+end if;
+end;
+$$ Language plpgsql;
+
+CREATE TRIGGER reservation_insert_update BEFORE insert or update on reservations for each row execute procedure reservation_insert_update();
+
+CREATE FUNCTION checkouts_insert_update() RETURNS "trigger" AS $$
+declare tomebook record;
+BEGIN
+	select into tomebook * from tomebooks where id = NEW.tomebook;
+IF tomebooks_available_to_reserve(tomebook.isbn, tomebook.library, NEW.semester) - tomebooks_reserved(tomebook.isbn, tomebook.library, NEW.semester) >= 1 THEN
+return NEW;
+else
+raise exception 'All available books are already reserved';
+end if;
+end;
+$$ Language plpgsql;
+
+CREATE TRIGGER checkouts_insert_update BEFORE insert or update on checkouts for each row execute procedure checkouts_insert_update();
