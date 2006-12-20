66e7dabf06f0301eefb6dd23084eaeb819746510
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:27:05 +0000

    r417@calefactor (orig r94):  fjord | 2006-06-27 08:48:09 -0500   r185@calefactor (orig r109):  cghawthorne | 2006-06-25 14:52:07 -0500   automated release tagging

---
+++ tag-release.sh
@@ -0,0 +1,2 @@
+svn rm -m "Tagging Release" svn+ssh://cghawthorne@remnetworks.org/home/cghawthorne/svn/tome/RELEASE
+svn cp -m "Tagging Release" svn+ssh://cghawthorne@remnetworks.org/home/cghawthorne/svn/tome/TRUNK svn+ssh://cghawthorne@remnetworks.org/home/cghawthorne/svn/tome/RELEASE
