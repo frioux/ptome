2216100ca82ac64d4fd1eca7518f02d729a016ee
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:56:19 +0000

    r595@calefactor (orig r208):  frew | 2006-10-19 13:34:26 -0500  Fixed bug in locking of database that didn't exist yet

--- TRUNK/scripts/database_migrations/01_ISBNvsID.sql
+++ TRUNK/scripts/database_migrations/01_ISBNvsID.sql
@@ -1,5 +1,5 @@
 BEGIN WORK;
-LOCK TABLE checkouts,tomebooks,reservations;
+LOCK TABLE checkouts,tomebooks;
 alter TABLE checkouts alter column tomebook type int;
 alter TABLE checkouts alter column semester type int;
 ALTER TABLE tomebooks alter column expire type integer;
