49ba474470d5e7992805ba95de1e96f7eef3b12d
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-20 05:36:43 +0000

    r462@calefactor (orig r129):  fjord | 2006-09-15 17:59:16 -0500   r461@calefactor:  cghawthorne | 2006-09-15 23:54:10 -0500   more work on the transition

--- TRUNK/scripts/ISBNvsID.sql
+++ TRUNK/scripts/ISBNvsID.sql
@@ -1,1 +1,12 @@
 create table reservations (id serial primary key, isbn varchar(20) references books not null, uid int references users (id) not null, patron int references patrons (id) not null, reserved timestamp with time zone not null default now(), fulfilled timestamp with time zone, comment text, library int references libraries (id) not null, semester int references semesters (id) not null);
+
+insert into reservations (isbn,uid,patron,reserved,comment,library,semester) select isbn, uid, borrower as patron, checkout as reserved, checkouts.comments as comment, checkouts.library as library, semester from checkouts,tomebooks where reservation = true AND checkouts.tomebook = tomebooks.id;
+
+delete from checkouts where reservation = true;
+drop INDEX one_borrower;
+drop INDEX one_reservation;
+alter table checkouts drop constraint checkout_or_reservation;
+ALTER TABLE checkouts drop column reservation;
+CREATE UNIQUE INDEX one_borrower ON checkouts (tomebook) WHERE checkin IS NULL;
+ALTER TABLE reservations ADD check(fulfilled IS NULL OR fulfilled > reserved);
+
