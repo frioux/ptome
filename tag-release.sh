26a4f5eacde3c39537ff25354f3955283efeaa36
cghawthorne <cghawthorne@e3db57ea-0125-0410-9136-e7560b3bc860>
2006-12-21 02:26:42 +0000

    it was a silly script anyway

--- tag-release.sh
+++
@@ -1,2 +0,0 @@
-svn rm -m "Tagging Release" http://fjord.dnsalias.org/svn/tome/RELEASE
-svn cp -m "Tagging Release" http://fjord.dnsalias.org/svn/tome/TRUNK http://fjord.dnsalias.org/svn/tome/RELEASE
