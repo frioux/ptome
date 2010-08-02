1bb60c44fb3a230770757e93655e23696d109458
johnboren <johnboren@e3db57ea-0125-0410-9136-e7560b3bc860>
2010-08-02 14:19:49 +0000

    Correct version number in the output (because SVN versioning is, sadly, not like GIT)

--- trunk/admin/scripts/pushLive.sh
+++ trunk/admin/scripts/pushLive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -f tome.tar.gz
 cd ../../
-export BUILD_NUM=`svn info | grep "Last Changed Rev" | awk -F' ' '{print $4}'`
+export BUILD_NUM=`svn info -r HEAD | grep "Last Changed Rev" | awk -F' ' '{print $4}'`
 cp header.php header.php.orig
 sed 's/\[\[BUILD_TIME\]\]/'$BUILD_NUM'/g' header.php.orig > header.php
 tar -czf tome.tar.gz *
