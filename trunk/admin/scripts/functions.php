9ba019508ea02ce9baa71cf28f3b8e3bb76d0819
johnboren <johnboren@e3db57ea-0125-0410-9136-e7560b3bc860>
2010-08-26 21:17:29 +0000

    Checkout forms behave properly now.

--- trunk/admin/scripts/functions.php
+++ trunk/admin/scripts/functions.php
@@ -142,13 +142,13 @@
             Reserved: <?php print date('m/d/y', strtotime($checkout["reserved"])); ?>
         <?php } ?>
         <br>
-        <div class="print-no" id="checkout<?php print $checkout["ID"]; ?>">
-            <form method="post" action="" onsubmit="new Ajax.Updater('checkout<?php print $checkout["ID"]; ?>','reserve.php', {
-                                    parameters: Form.serialize(this)+'&id=<?php print $checkout["ID"]; ?>',
-                                    onCreate: function(request){$('checkout<?php print $checkout["ID"]; ?>').innerHTML = 'Loading...'},
-                                    onSuccess: function(request){new Effect.Highlight( 'checkout<?php print $checkout["ID"]; ?>', { duration:0.5 } )}
+        <div class="print-no" id="checkout<?php print $checkout["checkoutID"]; ?>">
+            <form method="post" action="" onsubmit="new Ajax.Updater('checkout<?php print $checkout["checkoutID"]; ?>','reserve.php', {
+                                    parameters: Form.serialize(this)+'&id=<?php print $checkout["checkoutID"]; ?>',
+                                    onCreate: function(request){$('checkout<?php print $checkout["checkoutID"]; ?>').innerHTML = 'Loading...'},
+                                    onSuccess: function(request){new Effect.Highlight( 'checkout<?php print $checkout["checkoutID"]; ?>', { duration:0.5 } )}
                                     }); return false">
-                <input type="hidden" value="fill" name="type" id="checkouthidden<?php print $checkout["ID"]; ?>">
+                <input type="hidden" value="fill" name="type" id="checkouthidden<?php print $checkout["checkoutID"]; ?>">
                 <table class="noborder close">
                     <tbody>
                         <tr>
@@ -170,10 +170,10 @@
                         </tr>
                         <tr>
                             <td>
-                                <input type="submit" name="submit" value="Cancel" onclick="$('checkouthidden<?php print $checkout["ID"]; ?>').value='cancel';">
+                                <input type="submit" name="submit" value="Cancel" onclick="$('checkouthidden<?php print $checkout["checkoutID"]; ?>').value='cancel';">
                             </td>
                             <td>
-                                <input type="submit" name="submit" value="Fill Reservation" onclick="$('checkouthidden<?php print $checkout["ID"]; ?>').value='submit';">
+                                <input type="submit" name="submit" value="Fill Reservation" onclick="$('checkouthidden<?php print $checkout["checkoutID"]; ?>').value='submit';">
                             </td>
                         </tr>
                     </tbody>
