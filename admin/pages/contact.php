fed3d9c7f4c5e604bb6f53f95041171da31d687c
bionoren <bionoren@e3db57ea-0125-0410-9136-e7560b3bc860>
2011-09-10 21:15:23 +0000

    Fixes for the contact form.

--- admin/pages/contact.php
+++ admin/pages/contact.php
@@ -1,5 +1,6 @@
 <?php
     $path = "../../";
+    $pageTitle = "Contact";
     if(!isset($_GET["login"])) {
         require_once($path."header.php");
     } else {
@@ -12,7 +13,7 @@
             $headers = 'From: '.$_POST["from"]."\r\n".
             'Reply-To: '.$_POST["from"];
         }
-        mail("bion@drewcrawfordapps.com", "TOME", $_POST["comment"], $headers);
+        mail("bion@drewcrawfordapps.com", "TOME", $_REQUEST["session"].$_POST["comment"], $headers);
         header("Location:".$path."index.php");
     }
 ?>
@@ -21,7 +22,7 @@ Having problems? Contact a TOME developer!
 <br>
 <br>
 <form method="post" action="">
-    <input type="hidden" name="session" value="<?php var_dump($_SESSION); ?>">
+    <input type="hidden" name="session" value="<?php var_export($_SESSION, true); ?>">
     Your Email:
     <br>
     <input type="text" name="from"> (If you don't provide this, we can't respond to your questions)
